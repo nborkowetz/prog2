2c627a6e05f3026b1a366feed4b3633dc4a9c072
nicola <nborkowetz@gmail.com>
2021-03-20 16:50:29 +0100

    code modified with POSIX IO functions

--- serie1/s1e3.c
+++ serie1/s1e3.c
@@ -1,6 +1,3 @@
-//test push 
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -15,7 +12,18 @@ char *findExitPattern(char string[]){
     const char PATTERN[] = "exit";
     
     return strstr(string, PATTERN);
-} 
+}
+
+void removeNewLine(char *string){
+    int i = 0;
+
+    while(*(string + i) != '\n'){
+        i++;
+    }
+
+    *(string + i) = '\0';
+}
+
 
 int main(int argc, char *argv[]){
     pid_t pid[2];
@@ -31,62 +39,66 @@ int main(int argc, char *argv[]){
     int descriptor[2], buffer_size = 100;
     int status;
 
-    //creazione dei filgli
+    //creazione dei figli
     pid[0] = fork();
     
 
     if(pid[0] < 0 ){
-        printf("error \n");
+        write(STDOUT_FILENO, "error \n", 8);
         exit(EXIT_FAILURE);
     } 
     else if(pid[0] == 0 ) {
         status = execv("/usr/bin/xterm", arg);
         if(status == -1){
-            printf("error %d with execv \n", errno);
+            write(STDOUT_FILENO, "error with execv\n", 81);
         }
     }
     else {
         pid[1] = fork();
 
         if(pid[1] < 0 ){
-            printf("error \n");
+            write(STDOUT_FILENO, "error \n", 8);
             exit(EXIT_FAILURE);
         } 
         else if(pid[1] == 0 ) {
             status = execv("/usr/bin/xterm", arg);
             if(status == -1){
-                printf("error %d with execv \n", errno);
+                write(STDOUT_FILENO, "error with execv\n", 18);
             }
         }
         else{
             // terminale per la scrittura dei comandi 
-            printf("insert first device: ");  //sostituire con IO POSIX
-            scanf("%s", dev0);
+            write(STDOUT_FILENO, "insert first device: ", 22);
+            read(STDIN_FILENO, &dev0, 20);
+            removeNewLine(dev0);
+
             descriptor[0] = open(dev0, O_RDONLY);
 
             if(descriptor[0] < 0){
-                printf("error \n");
+                write(STDOUT_FILENO, "error \n", 8);
                 exit(EXIT_FAILURE);
             }
 
             // terminale per la visulalizzazione dei comandi 
-            printf("insert second device: ");  //sostituire con IO POSIX
-            scanf("%s", dev1);
+            write(STDOUT_FILENO, "insert second device: ", 22);
+            read(STDIN_FILENO, &dev1, 20);
+            removeNewLine(dev1);
+
             descriptor[1] = open(dev1, O_RDWR);
 
             if(descriptor[1] < 0){
-                printf("error \n");
+                write(STDOUT_FILENO, "error \n", 8);
                 exit(EXIT_FAILURE);
             }
 
             if(tcgetattr(descriptor[0], &attr0) == 0){
                 attr0.c_lflag |= ICANON;
                 if(tcsetattr(descriptor[0], TCSADRAIN, &attr0) != 0){
-                    printf("error \n");
+                    write(STDOUT_FILENO, "error \n", 8);
                     exit(EXIT_FAILURE);
                 }
             } else {
-                printf("error \n");
+                write(STDOUT_FILENO, "error \n", 8);
                 exit(EXIT_FAILURE);
             }
 
@@ -96,18 +108,16 @@ int main(int argc, char *argv[]){
                 bytes_read = read(descriptor[0], &buffer, buffer_size);
                 if(bytes_read > 0){
                     if(findExitPattern(buffer) == NULL){
-                        printf ("read %d byte from %s \n", bytes_read, dev0);
                         write(descriptor[1], &buffer, bytes_read);
-                        printf ("wrote %d byte on %s \n", bytes_read, dev1);
                     } else {
-                        printf("waiting for all the children to finish... \n");
+                        write(STDOUT_FILENO, "waiting for all the children to finish... \n", 44);
                         
                         //wait for all the children
                         for(int i = 0; i < pid_nr; i++){
                             waitpid(pid[i], NULL, 0);
                         }
 
-                        printf("Exiting... \n");
+                        write(STDOUT_FILENO, "Exiting... \n", 13);
                         exit(EXIT_SUCCESS);
                     }
                 }
